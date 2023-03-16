42d1c2b05c7207ba72a27255c0c3ad4880079500
ipeks <ipeksenci@gmail.com>
2023-03-16 22:08:23 +0300

    y

---
+++ src/test/java/practice/Q1.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Q1 {
+    //3. Amazon sayfasina gidelim. https://www.amazon.com/
+    //4. Sayfa basligini(title) yazdirin
+    //5. Sayfa basliginin “Amazon” icerdigini test edin
+    //6. Sayfa adresini(url) yazdirin
+    //7. Sayfa url’inin “amazon” icerdigini test edin.
+    //8. Sayfa handle degerini yazdirin
+    //9. Sayfa HTML kodlarinda “alisveris” kelimesi gectigini test edin
+    //10. Sayfayi kapatin.
+
+    
+}
